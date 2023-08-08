7441c79368863fb1777ad74aa0294bc7e1d78d3b
ryan horowitz <ryan.w.horowitz@gmail.com>
2023-08-08 16:55:37 -0400

    editing the add a meal functionality to handle errors

--- client/src/components/Meals.js
+++ client/src/components/Meals.js
@@ -4,17 +4,24 @@ import UserContext from "../AppContext"
 
 function Meals() {
   const [newMeal, setNewMeal] = useState('')
+  const [errors, setErrors] = useState([])
   const { meals } = useContext(UserContext)
 
-  function addAMeal() {
+  async function addAMeal() {
     console.log('JSON is ', JSON.stringify(newMeal))
-    fetch(`/meals`, {
+    const response = await fetch(`/meals`, {
       method: "POST",
       headers: {
         "Content-Type": "application/json"
       },
       body: JSON.stringify({ name: newMeal })
     })
+    const newMeal = await response.json()
+    if (response.ok) {
+      // update meals list
+    } else {
+      setErrors(Object.values(newMeal.errors))
+    }
   }
   const mealsList = <ul>
     {meals.map(meal => {
