245c41c5b9fd48c95250634140f26aed1e4f80cf
ryan horowitz <ryan.w.horowitz@gmail.com>
2023-08-08 10:35:16 -0400

    working on deploying successfully

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,10 @@
 import React, { useContext, useEffect } from 'react';
+import { Routes, Route, Link, useNavigate } from 'react-router-dom';
 import AppContext from './AppContext'
 import Login from "./components/Login";
 function App() {
+  const { user, setUser } = useContext(AppContext)
+  const navigate = useNavigate()
   return (
     <div className="App">
       <Login />
