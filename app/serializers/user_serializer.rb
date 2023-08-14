5ea07c271a6619d61216d9912830918ab2551f13
ryan horowitz <ryan.w.horowitz@gmail.com>
2023-08-14 12:48:24 -0400

    made a serializer method for recipe_by_meals

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,4 +1,19 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :username, :meals
+  attributes :id, :username, :meals, :recipes_by_meal
   has_many :recipes
+
+  # method that returns distinct meals with their recipes nested in them
+  def recipes_by_meal
+    organized_recipes = {}
+
+    object.meals.each do |meal|
+      organized_recipes[meal.name] ||= []
+    end
+
+    object.recipes.each do |recipe|
+      organized_recipes[recipe.meal_type] << recipe
+    end
+
+    organized_recipes
+  end
 end
